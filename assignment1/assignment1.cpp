26ca2adb7aa7646f750dd50db97fe55d5ee62bc6
Cao Phuc <caophuc799@gmail.com>
2018-04-11 15:28:47 +0700

    add main mesh

--- assignment1/assignment1.cpp
+++ assignment1/assignment1.cpp
@@ -7,6 +7,23 @@
 #include <math.h>
 #include <gl.h>
 #include <glut.h>
+#include <iostream>
+
+
+
+//Định nghĩa
+//----------------------------------------------------------------------------------------------
+#define PI			3.1415926
+#define	COLORNUM		14
+
+
+float	ColorArr[COLORNUM][3] = {{1.0, 0.0, 0.0}, {0.0, 1.0, 0.0}, { 0.0,  0.0, 1.0}, 
+								{1.0, 1.0,  0.0}, { 1.0, 0.0, 1.0},{ 0.0, 1.0, 1.0}, 
+								 {0.3, 0.3, 0.3}, {0.5, 0.5, 0.5}, { 0.9,  0.9, 0.9},
+								{1.0, 0.5,  0.5}, { 0.5, 1.0, 0.5},{ 0.5, 0.5, 1.0},
+									{0.0, 0.0, 0.0}, {1.0, 1.0, 1.0}};
+
+
 
 
 //----------------------------------------------------------------------------------------------
@@ -131,3 +148,346 @@ void Vector3::normalize()
 	y = y/temp;
 	z = z/temp;
 }
+
+
+
+//----------------------------------------------------------------------------------------------
+//Định nghĩa class VertexID
+
+
+class VertexID
+{
+public:
+	int		vertIndex;
+	int		colorIndex;
+};
+
+//----------------------------------------------------------------------------------------------
+//Định nghĩa class Face
+class Face
+{
+public:
+	int		nVerts;
+	VertexID*	vert;
+	
+	Face()
+	{
+		nVerts	= 0;
+		vert	= NULL;
+	}
+	~Face()
+	{
+		if(vert !=NULL)
+		{
+			delete[] vert;
+			vert = NULL;
+		}
+		nVerts = 0;
+	}
+};
+
+//----------------------------------------------------------------------------------------------
+//Định nghĩa class Mesh
+class Mesh
+{
+public:
+	int		numVerts;
+	Point3*		pt;
+	
+	int		numFaces;
+	Face*		face;
+public:
+	Mesh()
+	{
+		numVerts	= 0;
+		pt		= NULL;
+		numFaces	= 0;
+		face		= NULL;
+	}
+	~Mesh()
+	{
+		if (pt != NULL)
+		{
+			delete[] pt;
+		}	
+		if(face != NULL)
+		{
+			delete[] face;
+		}
+		numVerts = 0;
+		numFaces = 0;
+	}
+	void DrawWireframe();
+	void DrawColor();
+
+	void CreateTetrahedron();
+	void CreateCube(float	fSize);
+};
+
+
+void Mesh::CreateCube(float	fSize)
+{
+	int i;
+
+	numVerts=8;
+	pt = new Point3[numVerts];
+	pt[0].set(-fSize, fSize, fSize);
+	pt[1].set( fSize, fSize, fSize);
+	pt[2].set( fSize, fSize, -fSize);
+	pt[3].set(-fSize, fSize, -fSize);
+	pt[4].set(-fSize, -fSize, fSize);
+	pt[5].set( fSize, -fSize, fSize);
+	pt[6].set( fSize, -fSize, -fSize);
+	pt[7].set(-fSize, -fSize, -fSize);
+
+
+	numFaces= 6;
+	face = new Face[numFaces];
+
+	//Left face
+	face[0].nVerts = 4;
+	face[0].vert = new VertexID[face[0].nVerts];
+	face[0].vert[0].vertIndex = 1;
+	face[0].vert[1].vertIndex = 5;
+	face[0].vert[2].vertIndex = 6;
+	face[0].vert[3].vertIndex = 2;
+	for(i = 0; i<face[0].nVerts ; i++)
+		face[0].vert[i].colorIndex = 0;
+	
+	//Right face
+	face[1].nVerts = 4;
+	face[1].vert = new VertexID[face[1].nVerts];
+	face[1].vert[0].vertIndex = 0;
+	face[1].vert[1].vertIndex = 3;
+	face[1].vert[2].vertIndex = 7;
+	face[1].vert[3].vertIndex = 4;
+	for(i = 0; i<face[1].nVerts ; i++)
+		face[1].vert[i].colorIndex = 1;
+
+	//top face
+	face[2].nVerts = 4;
+	face[2].vert = new VertexID[face[2].nVerts];
+	face[2].vert[0].vertIndex = 0;
+	face[2].vert[1].vertIndex = 1;
+	face[2].vert[2].vertIndex = 2;
+	face[2].vert[3].vertIndex = 3;
+	for(i = 0; i<face[2].nVerts ; i++)
+		face[2].vert[i].colorIndex = 2;
+
+	//bottom face
+	face[3].nVerts = 4;
+	face[3].vert = new VertexID[face[3].nVerts];
+	face[3].vert[0].vertIndex = 7;
+	face[3].vert[1].vertIndex = 6;
+	face[3].vert[2].vertIndex = 5;
+	face[3].vert[3].vertIndex = 4;
+	for(i = 0; i<face[3].nVerts ; i++)
+		face[3].vert[i].colorIndex = 3;
+
+	//near face
+	face[4].nVerts = 4;
+	face[4].vert = new VertexID[face[4].nVerts];
+	face[4].vert[0].vertIndex = 4;
+	face[4].vert[1].vertIndex = 5;
+	face[4].vert[2].vertIndex = 1;
+	face[4].vert[3].vertIndex = 0;
+	for(i = 0; i<face[4].nVerts ; i++)
+		face[4].vert[i].colorIndex = 4;
+
+	//Far face
+	face[5].nVerts = 4;
+	face[5].vert = new VertexID[face[5].nVerts];
+	face[5].vert[0].vertIndex = 3;
+	face[5].vert[1].vertIndex = 2;
+	face[5].vert[2].vertIndex = 6;
+	face[5].vert[3].vertIndex = 7;
+	for(i = 0; i<face[5].nVerts ; i++)
+		face[5].vert[i].colorIndex = 5;
+
+}
+
+
+void Mesh::CreateTetrahedron()
+{
+	int i;
+	numVerts=4;
+	pt = new Point3[numVerts];
+	pt[0].set(0, 0, 0);
+	pt[1].set(1, 0, 0);
+	pt[2].set(0, 1, 0);
+	pt[3].set(0, 0, 1);
+
+	numFaces= 4;
+	face = new Face[numFaces];
+
+	face[0].nVerts = 3;
+	face[0].vert = new VertexID[face[0].nVerts];
+	face[0].vert[0].vertIndex = 1;
+	face[0].vert[1].vertIndex = 2;
+	face[0].vert[2].vertIndex = 3;
+	for(i = 0; i<face[0].nVerts ; i++)
+		face[0].vert[i].colorIndex = 0;
+	
+
+	face[1].nVerts = 3;
+	face[1].vert = new VertexID[face[1].nVerts];
+	face[1].vert[0].vertIndex = 0;	
+	face[1].vert[1].vertIndex = 2;
+	face[1].vert[2].vertIndex = 1;
+	for(i = 0; i<face[1].nVerts ; i++)
+		face[1].vert[i].colorIndex = 1;
+
+	
+	face[2].nVerts = 3;
+	face[2].vert = new VertexID[face[2].nVerts];
+	face[2].vert[0].vertIndex = 0;
+	face[2].vert[1].vertIndex = 3;
+	face[2].vert[2].vertIndex = 2;
+	for(i = 0; i<face[2].nVerts ; i++)
+		face[2].vert[i].colorIndex = 2;
+
+
+	face[3].nVerts = 3;
+	face[3].vert = new VertexID[face[3].nVerts];
+	face[3].vert[0].vertIndex = 1;
+	face[3].vert[1].vertIndex = 3;
+	face[3].vert[2].vertIndex = 0;
+	for(i = 0; i<face[3].nVerts ; i++)
+		face[3].vert[i].colorIndex = 3;
+}
+
+
+void Mesh::DrawWireframe()
+{
+	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+	for (int f = 0; f < numFaces; f++)
+	{
+		glBegin(GL_POLYGON);
+		for (int v = 0; v < face[f].nVerts; v++)
+		{
+			int		iv = face[f].vert[v].vertIndex;
+
+			glVertex3f(pt[iv].x, pt[iv].y, pt[iv].z);
+		}
+		glEnd();
+	}
+}
+
+void Mesh::DrawColor()
+{
+	glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+	for (int f = 0; f < numFaces; f++)
+	{
+		glBegin(GL_POLYGON);
+		for (int v = 0; v < face[f].nVerts; v++)
+		{
+			int		iv = face[f].vert[v].vertIndex;
+			int		ic = face[f].vert[v].colorIndex;
+			
+			ic = f % COLORNUM;
+
+			glColor3f(ColorArr[ic][0], ColorArr[ic][1], ColorArr[ic][2]); 
+			glVertex3f(pt[iv].x, pt[iv].y, pt[iv].z);
+		}
+		glEnd();
+	}
+}
+
+
+
+
+//Code main
+//-------------------------------------------------------------------------------------
+
+using namespace std;
+
+int		screenWidth = 600;
+int		screenHeight= 300;
+
+Mesh	tetrahedron;
+Mesh	cube;
+
+int		nChoice = 1;
+
+
+
+void drawAxis()
+{
+	glColor3f(0, 0, 1);
+	glBegin(GL_LINES);
+		glVertex3f(0, 0, 0);
+		glVertex3f(4, 0, 0);
+
+		glVertex3f(0, 0, 0);
+		glVertex3f(0, 4, 0);
+
+		glVertex3f(0, 0, 0);
+		glVertex3f(0, 0, 4);
+	glEnd();
+}
+void myDisplay()
+{
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	gluLookAt(4.5, 4, 2, 0, 0, 0, 0, 1, 0);
+
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);	
+	glViewport(0, 0, screenWidth/2, screenHeight);
+	
+	drawAxis();
+
+	glColor3f(0, 0, 0);
+	if(nChoice == 1)
+		tetrahedron.DrawWireframe();
+	else if(nChoice == 2)
+		cube.DrawWireframe();
+
+	glViewport(screenWidth/2, 0, screenWidth/2, screenHeight);
+
+	drawAxis();
+	if(nChoice == 1)
+		tetrahedron.DrawColor();
+	else if(nChoice == 2)
+		cube.DrawColor();
+
+	glFlush();
+    glutSwapBuffers();
+}
+
+void myInit()
+{
+	float	fHalfSize = 4;
+
+	glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+
+	glFrontFace(GL_CCW);
+	glEnable(GL_DEPTH_TEST);
+
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glOrtho(-fHalfSize, fHalfSize, -fHalfSize, fHalfSize, -1000, 1000);
+}
+
+int main(int argc, char* argv[])
+{
+	cout << "1. Tetrahedron" << endl;
+	cout << "2. Cube" << endl;
+	cout << "Input the choice: " << endl;
+	cin  >> nChoice;
+
+	glutInit(&argc, (char**)argv); //initialize the tool kit
+	glutInitDisplayMode(GLUT_DOUBLE |GLUT_RGB | GLUT_DEPTH);//set the display mode
+	glutInitWindowSize(screenWidth, screenHeight); //set window size
+	glutInitWindowPosition(100, 100); // set window position on screen
+	glutCreateWindow("Assignment1- Cao Phúc- 1512500"); // open the screen window
+
+	tetrahedron.CreateTetrahedron();
+	cube.CreateCube(1);
+
+	myInit();
+    glutDisplayFunc(myDisplay);
+	  
+	glutMainLoop();
+	return 0;
+}
+
